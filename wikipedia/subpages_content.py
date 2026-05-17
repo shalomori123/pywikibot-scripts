a4d9046b05fd5d0881f83955df1091ac9507d277
shalomori123 <81881844+shalomori123@users.noreply.github.com>
2026-05-17 23:41:05 +0300

    Update `subpages_content.py`: join references and regular pages to the same output

--- wikipedia/subpages_content.py
+++ wikipedia/subpages_content.py
@@ -2,19 +2,36 @@ import pywikibot
 from pywikibot.pagegenerators import PrefixingPageGenerator
 import re
 
-print('{{#בחר:{{{1}}}')
-
 temp = 'תבנית:דגל'
-for page in PrefixingPageGenerator(temp+'/'):
-	#if page.isRedirectPage():
-#		continue
-	text = page.text
-	text = re.sub('<noinclude>[\s\S]*?</noinclude>', '', text).strip()
-	name = page.title().replace(temp+'/', '')
-	if page.isRedirectPage():
-		print('|' + name + '|' + page.getRedirectTarget().title())
-	#if len(text) > 2000:
-#		print('skipping', name)
-#	else:
-#		print('|' + name + ' = ' + text)
-print('}}')
+head = '{{#בחר:{{{1}}}\n'
+tail = '}}'
+
+content = head
+references = dict() # target: [refs,]
+
+for page in PrefixingPageGenerator(prefix=temp + '/'):
+    name = page.title().replace(temp + '/', '')
+    
+    if page.isRedirectPage():
+        target = page.getRedirectTarget().title().replace(temp + '/', '')
+        if target in references:
+            references[target].append(name)
+        else:
+            references[target] = [name]
+        continue
+        
+    text = page.text
+    text = re.sub('<noinclude>[\s\S]*?(</noinclude>)?', '', text).strip()
+    
+    if len(text) > 2000:
+        print('skipping', name)
+        continue
+        
+    content += '|' + name + ' = ' + text + '\n'
+    
+content += tail
+
+for tar, refs in references.items():
+    content = content.replace('|' + tar + ' = ', '|' + '|'.join(refs) + '|' + tar + ' = ')
+
+print(content)
